73395c56020a8b43430f23c599ab451b8c9db35e
Shrey Choudhary <53032923+SxC97@users.noreply.github.com>
2021-04-14 22:29:45 -0500

    Add files via upload

---
+++ .hammerspoon/slowq.lua
@@ -0,0 +1,38 @@
+
+-- http://github.com/dbmrq/dotfiles/
+
+-- Requires you to keep holding Command + Q for a while before closing an app,
+-- so you won't do it accidentally.
+
+-- Replaces apps like CommandQ and SlowQuitApps.
+
+
+-----------------------------
+--  Customization Options  --
+-----------------------------
+
+local delay = 2.5 -- In seconds
+
+
+-------------------------------------------------------------------
+--  Don't mess with this part unless you know what you're doing  --
+-------------------------------------------------------------------
+
+local killedIt = false
+
+function pressedQ()
+    killedIt = false
+    hs.alert.show("⌘Q")
+    hs.timer.usleep(1000000 * delay)
+end
+
+function repeatQ()
+    if killedIt then return end
+    hs.application.frontmostApplication():kll()
+    killedIt = true
+    hs.alert.closeAll()
+end
+
+hs.hotkey.bind('cmd', 'Q', pressedQ, nil, repeatQ)
+
+i
